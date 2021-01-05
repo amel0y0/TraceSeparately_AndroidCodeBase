17eb47cdce40f7acb3bf893b85c3f1bba8db76a4
duberdeedu <xuehan008@gmail.com>
2021-01-05 20:52:53 +0800

    UUUpdate page

--- app/src/main/java/mdad/proj/deleteClient.java
+++ app/src/main/java/mdad/proj/deleteClient.java
@@ -1,6 +1,8 @@
 package mdad.proj;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class deleteClient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_client);
+
+        // my_toolbar is defined in the layout file
+        Toolbar myChildToolbar =
+                (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
