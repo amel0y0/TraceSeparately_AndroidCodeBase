de4fc2de34376cdf222ffb9c3a7d1d4f73208cae
amel0y0 <exelbert2010@gmail.com>
2021-01-08 00:24:01 +0800

    I am a Software Engineer and I don't care what people say!

--- app/src/main/java/mdad/proj/accelerometerService.java
+++ app/src/main/java/mdad/proj/accelerometerService.java
@@ -1,6 +1,7 @@
 package mdad.proj;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -17,39 +18,48 @@ public class accelerometerService extends Service implements SensorEventListener
 
     Handler handler = new Handler();
     Runnable runnable;
-    int delay = 10000;
-    private static final String TAG = "MyActivity";
+    int delay = 5000;
+    int delay2=1000;
+    private static final String TAG = "ACCELEROMETER SERVICE";
 
-    private SensorManager senSensorManager;
-    private Sensor senAccelerometer;
+    private SensorManager sensorManager;
+    Sensor accelerometer;
 
+    public float x,y,z;
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         return null;
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        x=event.values[0];
+        y=event.values[1];
+        z=event.values[2];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+        sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(accelerometerService.this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
         handler.postDelayed(runnable = new Runnable() {
             public void run() {
                 handler.postDelayed(runnable, delay);
-                Log.i(TAG,"LOG CAT 10 SECONDS");
-                Toast.makeText(accelerometerService.this, "This method is run every 2 seconds",
+                Log.d(TAG,"onSensorChanged X:" + x + "   Y:" + y + "   Z:" +z);
+                Toast.makeText(accelerometerService.this, "X:" + x + "Y:" + y + "Z:" +z,
                         Toast.LENGTH_SHORT).show();
             }
         }, delay);
         return START_STICKY;
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
 
 }
