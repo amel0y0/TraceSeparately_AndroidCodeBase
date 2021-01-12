2f6e2509ee8a94ab28553fe4470de7f55db5573e
amel0y0 <exelbert2010@gmail.com>
2021-01-12 15:45:55 +0800

    I am a Software Engineer and I don't care what people say!

--- app/src/main/java/mdad/proj/LogIn.java
+++ app/src/main/java/mdad/proj/LogIn.java
@@ -5,20 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LogIn extends AppCompatActivity {
     private EditText mPassword,mUserName;
     private Button btnSignIn;
     SharedPreferences sharedPreferences;
 
+    private String client_admin;
+
     public String  X = "john";
     public String Y = "password";
     public static final String Pref="JOHNNY";
     public static final String Name = "nameKey";
+
+
+
+
+    private static final String url_login = MainActivity.ipBaseAddress+"/loginJ.php";
     // url to update product
 
     // private static final String url_login = MainActivity.ipBaseAddress+"/php/loginJ.php";
@@ -26,6 +45,7 @@ public class LogIn extends AppCompatActivity {
     // JSON Node names
 
     private static final String TAG_SUCCESS = "success";
+    private static final String CLIENT_ADMIN = "client_admin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,4 +113,68 @@ public class LogIn extends AppCompatActivity {
         });
 
     }
+
+    public void postData(String url, final JSONObject json, final int option){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest json_obj_req = new JsonObjectRequest(
+                Request.Method.POST, url, json, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+
+                switch (option){
+                    case 1:checkResponseLogin(response); break;
+
+                }
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+//                String alert_message;
+//                alert_message = error.toString();
+//                showAlertDialogue("Error", alert_message);
+            }
+
+        });
+        requestQueue.add(json_obj_req);
+    }
+
+
+    public void checkResponseLogin(JSONObject response)
+    {
+        Log.i("----Response", response+" "+url_login);
+        try {
+            if(response.getInt(TAG_SUCCESS)==1){
+
+                //get CLIENT OR ADMIN STRING
+                client_admin=response.getString(CLIENT_ADMIN);
+
+                if(client_admin.equals("A")){
+                    //Go Admin Side
+                    finish();
+                    Intent i = new Intent(this, MainActivity.class);
+                    startActivity(i);
+                }
+                if(client_admin.equals("C")){
+                    //go Client Side
+                    finish();
+                    Intent i = new Intent(this, backendService.class);
+                    startActivity(i);
+                }
+
+            }else{
+                Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/mdad/proj/backendService.java
+++ app/src/main/java/mdad/proj/backendService.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class backendService extends AppCompatActivity {
 
     private Button btnService;
+    private static final String TAGBTN ="BUTTON PRESSED";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class backendService extends AppCompatActivity {
 
                     @Override
                     public void onClick(View v) {
+                        Log.i(TAGBTN,"SERVICE HAS BEEN STARTEDDDD");
                         startService(new Intent(backendService.this, accelerometerService.class));
                     }
                 }
