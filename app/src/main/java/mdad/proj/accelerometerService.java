42c53eb8e182c8e70511e5affecfd46c9ed4ea5b
amel0y0 <exelbert2010@gmail.com>
2021-02-02 13:59:16 +0800

    I am a Prodigy in the making #FutureGooglePinoyCEO

--- app/src/main/java/mdad/proj/accelerometerService.java
+++ app/src/main/java/mdad/proj/accelerometerService.java
@@ -3,6 +3,7 @@ package mdad.proj;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -18,6 +19,7 @@ public class accelerometerService extends Service implements SensorEventListener
 
     //private final static int INTERVAL = 1000 * 60 * 180 //3 hours
 
+    int reportER;
     Handler handler = new Handler();
     Runnable runnable;
     int delay =10000;
@@ -58,7 +60,61 @@ public class accelerometerService extends Service implements SensorEventListener
         handler.postDelayed(runnable = new Runnable() {
 
             public void run() {
+                int x_service, x_service_write;
+                SharedPreferences myPrefs = getSharedPreferences("SERVICE_START",0);
+                SharedPreferences.Editor myEditor = myPrefs.edit();
+                x_service= myPrefs.getInt("SERVICE_START",0);
+                handler.postDelayed(runnable, delay); // DELAY
 
+
+                if(x_service>0)
+                {
+                    float check1,check2;
+                    float x2,y2,z2;
+                    Log.i(TAG,"onSensorChanged X:" + x + "   Y:" + y + "   Z:" +z);
+                    Date currentTime = Calendar.getInstance().getTime();
+                    Log.i(TIME_TAG,"Current Time of Sending:" +currentTime);
+                    Toast.makeText(accelerometerService.this, "X:" + x + "Y:" + y + "Z:" +z, Toast.LENGTH_SHORT).show();
+                    /**
+                     *  POST DATA HERE
+                     */
+
+                    x2=myPrefs.getFloat("X_VALUE",0);
+                    y2=myPrefs.getFloat("Y_VALUE",0);
+                    z2=myPrefs.getFloat("Z_VALUE",0);
+
+                    check1=x+y+z;
+                    check2=x2+y2+z2;
+
+                    if(check1!=check2)
+                    {
+                        myEditor.putFloat("X_VALUE",x);
+                        myEditor.putFloat("Y_VALUE",y);
+                        myEditor.putFloat("Z_VALUE",z);
+                        myEditor.commit();
+
+                        if(reportER>0)
+                            reportER--;
+
+                        Log.i("CHECK_CHECK","CHECK 1 and 2 ARE NOT THE SAME ");
+                    }
+                    else {
+                        sendToReport(reportER++);
+                        Log.i("CHECK CHECK SAME", "CURRENT DATA:"+reportER);
+                    }
+
+                }
+                else if(x_service == 0)
+                {
+                     x_service_write=x_service+1;
+                    Log.i("ADDED DATA", "SESSION SERVICE POSITION:" +x_service_write);
+
+                    myEditor.putInt("SERVICE_START",x_service_write);
+                    myEditor.putFloat("X_VALUE",x);
+                    myEditor.putFloat("Y_VALUE",y);
+                    myEditor.putFloat("Z_VALUE",z);
+                    myEditor.commit();
+                }
                 /**
                  *     CALL FOR PHP SESSION and CHECK WITH IF ELSE STATEMENT IF WHETHER OR NOT SHOULD SEND TO DATABASE OR NOT.
                  *     IF(x>x+1);
@@ -69,16 +125,30 @@ public class accelerometerService extends Service implements SensorEventListener
                  *
                  *     WE WILL BE USING 2 PHP FILES FOR THE SESSION AND CHANGING ALL.
                  */
-                handler.postDelayed(runnable, delay);
-                Log.i(TAG,"onSensorChanged X:" + x + "   Y:" + y + "   Z:" +z);
-                Date currentTime = Calendar.getInstance().getTime();
-                Log.i(TIME_TAG,"Current Time of Sending:" +currentTime);
-                Toast.makeText(accelerometerService.this, "X:" + x + "Y:" + y + "Z:" +z, Toast.LENGTH_SHORT).show();
             }
         }, delay);
         return START_STICKY;
 
     }
 
+    public void sendData(){
+        /**
+         *
+         *      CALL SEND UPDATES TABLE
+         */
+
+    }
+
+    public void sendToReport(int reportER){
+
+        if(reportER>5 )
+        {
+
+            Log.i("REPORTER_STATUS","TIME TO MESSAGE THE ADMIN");
+            /**
+             * Call for the Admin
+              */
+        }
+    }
 
 }
--- app/src/main/java/mdad/proj/backendService.java
+++ app/src/main/java/mdad/proj/backendService.java
@@ -3,6 +3,7 @@ package mdad.proj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -27,6 +28,16 @@ public class backendService extends AppCompatActivity {
                     public void onClick(View v) {
                         Log.i(TAGBTN,"SERVICE HAS BEEN STARTEDDDD");
 
+                        /**
+                         * Shows that it Service just started so unable to send data first. Only when X is more than previous session then can say that send data lorh
+                         */
+
+                        int x=0;
+
+                        SharedPreferences myPrefs = getSharedPreferences("SERVICE_START",0);
+                        SharedPreferences.Editor myEditor = myPrefs.edit();
+                        myEditor.putInt("SERVICE_START",x);
+                        myEditor.commit();
                         startService(new Intent(backendService.this, accelerometerService.class));
                     }
                 }
